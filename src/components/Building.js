59afaad00bccbab8863d82472931a462369bfc41
babivillanova <77370233+babivillanova@users.noreply.github.com>
2022-06-05 12:42:13 -0300

    dashboard

--- src/components/Building.js
+++ src/components/Building.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './Footer.css';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
-import Foot from './images/Footer.png'; 
+import Building from './images/building.png'; 
 
-function Footer() {
+function Build() {
   return (
 
       <div class='social-media'>
         <div class='social-media-wrap'>
-        <img className='footer' src={Foot} alt='blas'/> 
+        <img className='build' src={Building} alt='blas'/> 
         </div>
       </div>
 
   );
 }
 
-export default Footer;
+export default Build;
--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -11,8 +11,7 @@ export default function Dashboard() {
       <>
         <Dash/>
         <Building/>
-   
-        <Footer/>
+           <Footer/>
   
       </>
     );
